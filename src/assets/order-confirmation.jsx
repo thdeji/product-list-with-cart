cfe8dc202113d1d22e3fd73b55e846025e144075
thdeji <omotobaadedeji@gmail.com>
2025-12-14 01:11:48 +0100

    dffdf

--- src/assets/order-confirmation.jsx
+++ src/assets/order-confirmation.jsx
@@ -1,63 +1,69 @@
 import { useEffect } from "react";
+import orderConfirmedIcon from "./images/icon-order-confirmed.svg";
+
 function OrderConfirmation({ cart, orderTotal }) {
-   useEffect(() => {
-     document.body.classList.add("overflow-hidden");
-     return () => {
-       document.body.classList.remove("overflow-hidden");
-     };
-   }, []);
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []);
+
   return (
     <div>
-      <div className=" max-h-full bg-black/50 fixed inset-0 lg:flex lg:items-center lg:justify-center md:flex md:items-center md:justify-center z-50">
+      <div className="max-h-full bg-black/50 fixed inset-0 lg:flex lg:items-center lg:justify-center md:flex md:items-center md:justify-center z-50">
         <div className="bg-Rose100 flex flex-col gap-10 p-10 w-full md:w-auto rounded-2xl max-h-[80vh] overflow-y-auto fixed bottom-0 md:static md:my-auto">
-          <img
-            src="./images/icon-order-confirmed.svg"
-            alt=""
-            srcset=""
-            className="w-20"
-          />
+          <img src={orderConfirmedIcon} className="w-20" />
+
           <div className="text-left">
             <h1 className="text-Rose900 font-redHatBold text-4xl mb-2">
-              Order<br/> Confirmed
+              Order
+              <br />
+              Confirmed
             </h1>
             <p className="text-Rose400 font-redHatRegular">
               We hope you enjoy your food!
             </p>
           </div>
+
           <div>
-          {cart.map((item) => (
-            <div key={item.id}>
-              <div className="flex justify-between mb-4">
-                <div className="flex gap-4">
-              <img src={item.thumbNail} alt="" className="h-[50%]"/>
-                <div className="flex flex-col gap-1">
-                  <div className="font-redHatSemibold text-Rose900">
-                    {item.itemName}
-                  </div>
+            {cart.map((item) => (
+              <div key={item.id}>
+                <div className="flex justify-between mb-4">
                   <div className="flex gap-4">
-                    <div className="font-redHatSemibold text-thisRed">
-                      {item.quantity}x
-                    </div>
-                    <div className="font-redHatSemibold text-Rose300">
-                      @ ${item.price}
+                    <img src={item.thumbNail} className="h-[50%]" />
+                    <div className="flex flex-col gap-1">
+                      <div className="font-redHatSemibold text-Rose900">
+                        {item.itemName}
+                      </div>
+                      <div className="flex gap-4">
+                        <div className="font-redHatSemibold text-thisRed">
+                          {item.quantity}x
+                        </div>
+                        <div className="font-redHatSemibold text-Rose300">
+                          @ ${item.price}
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div></div>
-                <div className="font-redHatSemibold text-Rose400">
-                  ${item.quantity * item.price}
+                  <div className="font-redHatSemibold text-Rose400">
+                    ${item.quantity * item.price}
+                  </div>
                 </div>
               </div>
+            ))}
+
+            <div className="flex justify-between mx-4">
+              <p className="font-redHatRegular text-Rose500">Order Total</p>
+              <p className="font-redHatBold text-2xl text-Rose900">
+                ${orderTotal}
+              </p>
             </div>
-          ))}
-          <div className="flex justify-between mx-4">
-            <p className="font-redHatRegular text-Rose500">Order Total</p>
-            <p className="font-redHatBold text-2xl text-Rose900">
-              ${orderTotal}
-            </p>
-          </div></div>
+          </div>
+
           <button
             className="bg-thisRed font-redHatSemibold text-Rose50 rounded-full w-full p-4 hover:cursor-pointer"
-            onClick={()=> window.location.reload()}
+            onClick={() => window.location.reload()}
           >
             Start New Order
           </button>
@@ -66,4 +72,5 @@ function OrderConfirmation({ cart, orderTotal }) {
     </div>
   );
 }
+
 export default OrderConfirmation;
--- src/assets/shop.jsx
+++ src/assets/shop.jsx
@@ -1,6 +1,47 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Item from "./item-component";
 import OrderConfirmation from "./order-confirmation";
+
+import waffleMobile from "./images/image-waffle-mobile.jpg";
+import waffleDesktop from "./images/image-waffle-desktop.jpg";
+import waffleThumb from "./images/image-waffle-thumbnail.jpg";
+
+import cremeMobile from "./images/image-creme-brulee-mobile.jpg";
+import cremeDesktop from "./images/image-creme-brulee-desktop.jpg";
+import cremeThumb from "./images/image-creme-brulee-thumbnail.jpg";
+
+import macaronMobile from "./images/image-macaron-mobile.jpg";
+import macaronDesktop from "./images/image-macaron-desktop.jpg";
+import macaronThumb from "./images/image-macaron-thumbnail.jpg";
+
+import tiramisuMobile from "./images/image-tiramisu-mobile.jpg";
+import tiramisuDesktop from "./images/image-tiramisu-desktop.jpg";
+import tiramisuThumb from "./images/image-tiramisu-thumbnail.jpg";
+
+import baklavaMobile from "./images/image-baklava-mobile.jpg";
+import baklavaDesktop from "./images/image-baklava-desktop.jpg";
+import baklavaThumb from "./images/image-baklava-thumbnail.jpg";
+
+import meringueMobile from "./images/image-meringue-mobile.jpg";
+import meringueDesktop from "./images/image-meringue-desktop.jpg";
+import meringueThumb from "./images/image-meringue-thumbnail.jpg";
+
+import cakeMobile from "./images/image-cake-mobile.jpg";
+import cakeDesktop from "./images/image-cake-desktop.jpg";
+import cakeThumb from "./images/image-cake-thumbnail.jpg";
+
+import brownieMobile from "./images/image-brownie-mobile.jpg";
+import brownieDesktop from "./images/image-brownie-desktop.jpg";
+import brownieThumb from "./images/image-brownie-thumbnail.jpg";
+
+import pannaMobile from "./images/image-panna-cotta-mobile.jpg";
+import pannaDesktop from "./images/image-panna-cotta-desktop.jpg";
+import pannaThumb from "./images/image-panna-cotta-thumbnail.jpg";
+
+import emptyCart from "./images/illustration-empty-cart.svg";
+import removeIcon from "./images/icon-remove-item.svg";
+import carbonIcon from "./images/icon-carbon-neutral.svg";
+
 function Shop() {
   const [cart, setCart] = useState([]);
   const [isOrderConfirmed, setIsOrderConfirmed] = useState(false);
@@ -8,7 +49,6 @@ function Shop() {
   const addToCart = (product) => {
     setCart((prev) => {
       const exists = prev.find((item) => item.id === product.id);
-
       if (exists) {
         return prev.map((item) =>
           item.id === product.id
@@ -16,10 +56,10 @@ function Shop() {
             : item
         );
       }
-
       return [...prev, product];
     });
   };
+
   const removeItem = (id) => {
     setCart((prev) => prev.filter((item) => item.id !== id));
   };
@@ -28,6 +68,7 @@ function Shop() {
     (total, item) => total + Number(item.price * item.quantity),
     0
   );
+
   const quantityTotal = cart.reduce(
     (total, item) => total + Number(item.quantity),
     0
@@ -44,117 +85,116 @@ function Shop() {
   const handleOrderConfirmed = () => {
     setIsOrderConfirmed(true);
   };
-  const startNewOrder = () => {
-    setIsOrderConfirmed(false);
-  };
 
   return (
     <div>
       <h1 className="text-rose-950 font-redHatBold text-4xl text-left mb-3">
         Desserts
       </h1>
+
       <div className="lg:flex lg:gap-4 items-start">
         <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3">
           <Item
-            itemImage="./images/image-waffle-mobile.jpg"
-            itemImageDesktop="./images/image-waffle-desktop.jpg"
+            itemImage={waffleMobile}
+            itemImageDesktop={waffleDesktop}
+            thumbNail={waffleThumb}
             title="Waffle"
             itemName="Waffle with Berries"
             addToCart={addToCart}
             price="6.50"
             id={0}
             onQuantityChange={updateQuantity}
-            thumbNail="./images/image-waffle-thumbnail.jpg"
           />
           <Item
-            itemImage="./images/image-creme-brulee-mobile.jpg"
-            itemImageDesktop="./images/image-creme-brulee-desktop.jpg"
+            itemImage={cremeMobile}
+            itemImageDesktop={cremeDesktop}
+            thumbNail={cremeThumb}
             title="Crème Brûlée"
             itemName="Vanilla Bean Crème Brûlée"
-            price="7.00"
             addToCart={addToCart}
+            price="7.00"
             id={1}
             onQuantityChange={updateQuantity}
-            thumbNail="./images/image-creme-brulee-thumbnail.jpg"
           />
           <Item
-            itemImageDesktop="./images/image-macaron-desktop.jpg"
-            itemImage="./images/image-macaron-mobile.jpg"
+            itemImage={macaronMobile}
+            itemImageDesktop={macaronDesktop}
+            thumbNail={macaronThumb}
             title="Macaron"
             itemName="Macaron Mix of Five"
             addToCart={addToCart}
             price="8.00"
             id={5}
             onQuantityChange={updateQuantity}
-            thumbNail="./images/image-macaron-thumbnail.jpg"
           />
           <Item
-            itemImageDesktop="./images/image-tiramisu-desktop.jpg"
-            itemImage="./images/image-tiramisu-mobile.jpg"
+            itemImage={tiramisuMobile}
+            itemImageDesktop={tiramisuDesktop}
+            thumbNail={tiramisuThumb}
             title="Tiramisu"
             itemName="Classic Tiramisu"
             addToCart={addToCart}
             price="5.50"
             id={6}
             onQuantityChange={updateQuantity}
-            thumbNail="./images/image-tiramisu-thumbnail.jpg"
           />
           <Item
-            itemImageDesktop="./images/image-baklava-desktop.jpg"
-            itemImage="./images/image-baklava-mobile.jpg"
+            itemImage={baklavaMobile}
+            itemImageDesktop={baklavaDesktop}
+            thumbNail={baklavaThumb}
             title="Baklava"
             itemName="Pistachio Baklava"
             addToCart={addToCart}
             price="4.00"
             id={7}
             onQuantityChange={updateQuantity}
-            thumbNail="./images/image-baklava-thumbnail.jpg"
           />
           <Item
-            itemImageDesktop="./images/image-meringue-desktop.jpg"
-            itemImage="./images/image-meringue-mobile.jpg"
+            itemImage={meringueMobile}
+            itemImageDesktop={meringueDesktop}
+            thumbNail={meringueThumb}
             title="Pie"
             itemName="Lemon Meringue Pie"
             addToCart={addToCart}
             price="5.00"
             id={8}
             onQuantityChange={updateQuantity}
-            thumbNail="./images/image-meringue-thumbnail.jpg"
           />
           <Item
-            itemImageDesktop="./images/image-cake-desktop.jpg"
-            itemImage="./images/image-cake-mobile.jpg"
+            itemImage={cakeMobile}
+            itemImageDesktop={cakeDesktop}
+            thumbNail={cakeThumb}
             title="Cake"
             itemName="Red Velvet Cake"
             addToCart={addToCart}
             price="4.50"
             id={9}
             onQuantityChange={updateQuantity}
-            thumbNail="./images/image-cake-thumbnail.jpg"
           />
           <Item
-            itemImageDesktop="./images/image-brownie-desktop.jpg"
-            itemImage="./images/image-brownie-mobile.jpg"
+            itemImage={brownieMobile}
+            itemImageDesktop={brownieDesktop}
+            thumbNail={brownieThumb}
             title="Brownie"
             itemName="Salted Caramel Brownie"
             addToCart={addToCart}
             price="4.50"
             id={10}
             onQuantityChange={updateQuantity}
-            thumbNail="./images/image-brownie-thumbnail.jpg"
           />
           <Item
-            itemImageDesktop="./images/image-panna-cotta-desktop.jpg"
-            itemImage="./images/image-panna-cotta-mobile.jpg"
+            itemImage={pannaMobile}
+            itemImageDesktop={pannaDesktop}
+            thumbNail={pannaThumb}
             title="Panna Cotta"
             itemName="Vanilla Panna Cotta"
             addToCart={addToCart}
             price="6.50"
             id={11}
             onQuantityChange={updateQuantity}
-            thumbNail="./images/image-panna-cotta-thumbnail.jpg"
           />
         </div>
+
         <div className="bg-white p-6 rounded-2xl">
           <h1 className="font-redHatBold text-thisRed text-2xl mb-7">
             Your Cart ({quantityTotal})
@@ -162,64 +202,57 @@ function Shop() {
 
           {cart.length === 0 && (
             <div className="whitespace-nowrap font-redHatSemibold text-Rose900 flex flex-col gap-4 grow-0">
-              <img
-                src="./images/illustration-empty-cart.svg"
-                alt="illustration-empty-cart.svg"
-              />
+              <img src={emptyCart} />
               Your added items will appear here
             </div>
           )}
+
           {cart.length >= 1 && (
             <div className="flex flex-col gap-4 grow-0">
               {cart.map((item) => (
                 <div key={item.id}>
-                  {item.quantity >= 1 && (
-                    <div className="flex justify-between mx-4 mb-4 border-b border-Rose100">
-                      <div className="flex flex-col gap-1">
-                        <div className="font-redHatSemibold text-Rose900">
-                          {item.itemName}
-                        </div>
-                        <div className="flex gap-4 mb-4">
-                          <div className="font-redHatSemibold text-thisRed">
-                            {item.quantity}x
-                          </div>
-                          <div className="font-redHatSemibold text-Rose300">
-                            @ ${item.price}
-                          </div>
-                          <div className="font-redHatSemibold text-Rose400">
-                            ${item.quantity * item.price}
-                          </div>
-                        </div>
+                  <div className="flex justify-between mx-4 mb-4 border-b border-Rose100">
+                    <div className="flex flex-col gap-1">
+                      <div className="font-redHatSemibold text-Rose900">
+                        {item.itemName}
                       </div>
-                      <div>
-                        <img
-                          src="./images/icon-remove-item.svg"
-                          className="cursor-pointer border px-0.5 py-0.5 border-Rose300 rounded-2xl"
-                          alt="icon-remove-item"
-                          onClick={() => removeItem(item.id)}
-                        />
+                      <div className="flex gap-4 mb-4">
+                        <div className="font-redHatSemibold text-thisRed">
+                          {item.quantity}x
+                        </div>
+                        <div className="font-redHatSemibold text-Rose300">
+                          @ ${item.price}
+                        </div>
+                        <div className="font-redHatSemibold text-Rose400">
+                          ${item.quantity * item.price}
+                        </div>
                       </div>
                     </div>
-                  )}
+                    <img
+                      src={removeIcon}
+                      className="cursor-pointer border px-0.5 py-0.5 border-Rose300 rounded-2xl"
+                      onClick={() => removeItem(item.id)}
+                    />
+                  </div>
                 </div>
               ))}
+
               <div className="flex justify-between mx-4">
                 <p className="font-redHatRegular text-Rose500">Order Total</p>
                 <p className="font-redHatBold text-2xl text-Rose900">
                   ${orderTotal}
                 </p>
               </div>
+
               <div className="bg-Rose100 rounded p-4 flex justify-center gap-4 mx-4 font-redHatRegular text-Rose900 whitespace-nowrap">
-                <img
-                  src="./images/icon-carbon-neutral.svg"
-                  alt="icon-carbon-neutral"
-                />
+                <img src={carbonIcon} />
                 <p>
-                  This is a
-                  <span className="font-redHatBold"> carbon-neutral </span>
+                  This is a{" "}
+                  <span className="font-redHatBold">carbon-neutral</span>{" "}
                   delivery
                 </p>
               </div>
+
               <button
                 className="bg-thisRed font-redHatSemibold text-Rose50 rounded-full w-full p-4"
                 onClick={handleOrderConfirmed}
@@ -230,10 +263,12 @@ function Shop() {
           )}
         </div>
       </div>
+
       {isOrderConfirmed && quantityTotal > 0 && (
         <OrderConfirmation cart={cart} orderTotal={orderTotal} />
       )}
     </div>
   );
 }
+
 export default Shop;
